4b892d61553fc9468f7a5e0a1c2386d81cc1a1e7
chinthanshetty2005@gmail.com <putturvpt@gmail.com>
2023-08-14 14:48:23 +0530

    Division function added to arith.py

--- Python_Progs/arith.py.py
+++ Python_Progs/arith.py.py
@@ -7,11 +7,13 @@ return(n1-n2)
 def mul(n1,n2):
 return(n1*n2)
 
-
+def div(n1,n2):
+return(n1/n2)
 
 a=int(input('Enter num1:'))
 b=int(input('Enter num2:'))
 print('Sum=',add(a,b))
 print('Difference=',sub(a,b))
 print('Product=',mul(a,b))
+print('Quotient=',div(a,b))
 
