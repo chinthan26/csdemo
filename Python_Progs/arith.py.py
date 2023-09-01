401d037fbe9e408f0db8ff5e2a2c1908c83d7228
chaithan <chaithankumarscsk3@gmail.com>
2023-09-01 22:56:44 +0530

    Added division function to arith.py file

--- Python_Progs/arith.py.py
+++ Python_Progs/arith.py.py
@@ -12,8 +12,8 @@ return(n1/n2)
 
 a=int(input('Enter num1:'))
 b=int(input('Enter num2:'))
-print('Sum=',add(a,b))
-print('Difference=',sub(a,b))
-print('Product=',mul(a,b))
-print('Quotient=',div(a,b))
+print("Sum=,"add(a,b))
+print("Difference=",sub(a,b))
+print("Product=,"mul(a,b))
+print("Quotient=",div(a,b))
 
